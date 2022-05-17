752bc424c36ab1babe8be63fbf60b750cccfafd5
Lee Mount <lee@Lees-MacBook-Pro.local>
2022-05-16 20:43:14 -0700

    Added Navbar

---
+++ frontend/src/components/NavbarComponent.tsx
@@ -0,0 +1,36 @@
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { WalletInfo } from "./WalletInfo";
+
+export const NavbarComponent = ({}) => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">EVIX Protocol</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#trade">Trade</Nav.Link>
+            <Nav.Link href="#lp">Provide Liquidity</Nav.Link>
+            <NavDropdown title="FAQ" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#risks">Risks</NavDropdown.Item>
+              <NavDropdown.Item href="#strategies">
+                Trading Strategies
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#whitepaper">Whitepaper</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <WalletInfo/>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
---
+++ frontend/src/components/WalletInfo.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { ethers } from "ethers";
+
+import { Button, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const WalletInfo = ({}) => {
+    const [data, setdata] = useState({
+        address: "",
+        Balance: "",
+    })
+
+    const metamaskHandler = () => {
+        if(window.ethereum){
+            window.ethereum.request({method: "eth_requestAccounts" })
+            .then((res: any) => accountChangeHandler(res[0]))
+        }else{
+            alert("Please install metamask extension");
+        }
+    };
+
+    const getBalance = (address: any) => {
+        window.ethereum.request({
+            method: "eth_getBalance",
+            params: [address, "latest"]
+        })
+        .then((balance: any) => {
+            setdata({
+            address: address,
+            Balance: ethers.utils.formatEther(balance),
+            });
+        });
+    };
+
+    const accountChangeHandler = (account: any) => {
+        setdata({
+            address: account,
+            Balance: "",
+        });
+
+        getBalance(account);
+    }
+
+    return (
+        <Card className = "text-center">
+            <Card.Header className="float-end">
+                <strong>Address: </strong>
+                {data.address}
+            </Card.Header>
+
+            <Card.Body>
+                <Card.Text>
+                <strong>Balance: </strong>
+                {data.Balance} ETH
+                </Card.Text>
+
+                <Button onClick={metamaskHandler} variant="primary" className="float-end">
+                Connect to wallet
+                </Button>
+            </Card.Body>
+            
+        </Card>
+    )
+}
