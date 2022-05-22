0aafa3af4894e4d5eaf94f111d9d656dbf6122fb
Lee Mount <lee@Lees-MacBook-Pro.local>
2022-05-22 07:00:25 -0700

    added price & index to hoomepage

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { ethers } from "ethers";
 import usdcJson from "../abis/USDCMock.json";
+import oracleJson from "../abis/Oracle.json";
+import vpoolJson from "../abis/PerpVPool.json";
 
 import {
   Button,
@@ -21,12 +23,31 @@ const usdcAbi = usdcJson.abi;
 const usdc = new ethers.Contract(USDC_ADDRESS, usdcAbi, provider);
 
 const MARGINPOOL_ADDRESS = process.env.REACT_APP_MARGINPOOL_ADDRESS!;
+
+const ORACLE_ADDRESS = process.env.REACT_APP_ORACLE_ADDRESS!;
+const oracleAbi = oracleJson.abi;
+const oracle = new ethers.Contract(ORACLE_ADDRESS, oracleAbi, provider);
+
 const VPOOL_ADDRESS = process.env.REACT_APP_VPOOL_ADDRESS!;
+const vpoolAbi = vpoolJson.abi;
+const vpool = new ethers.Contract(VPOOL_ADDRESS, vpoolAbi, provider);
 
 export const HomeComponent = () => {
   const [depositedUSDC, updateDepositedUSDC] = useState({
     amount: 0,
   });
+  const [EVIXOraclePrice, updateEVIXOraclePrice] = useState();
+  const [EVIXPoolPrice, updateEVIXPoolPrice] = useState();
+
+  const GetEVIXIndexMark = async () => {
+    let val = await oracle.functions.spotEVIXLevel();
+    updateEVIXOraclePrice(val.toString());
+  };
+
+  const GetEVIXPoolPrice = async () => {
+    let val = await vpool.functions.price();
+    updateEVIXPoolPrice(val.toString());
+  };
 
   const GetDespositedTotal = async () => {
     let valMarginPool = await usdc.functions.balanceOf(MARGINPOOL_ADDRESS);
@@ -38,6 +59,8 @@ export const HomeComponent = () => {
   };
 
   GetDespositedTotal();
+  GetEVIXIndexMark();
+  GetEVIXPoolPrice();
 
   return (
     <Card style={{ width: "18rem" }} className="text-center me-5 mt-5">
@@ -45,6 +68,18 @@ export const HomeComponent = () => {
         <Card.Title>Total USDC Deposited</Card.Title>
         <Card.Text className="text-center">
           {depositedUSDC.amount} USDC
+        </Card.Text>
+      </Card.Body>
+      <Card.Body>
+        <Card.Title>Current EVIX Index Level</Card.Title>
+        <Card.Text className="text-center">
+          {EVIXOraclePrice}
+        </Card.Text>
+      </Card.Body>
+      <Card.Body>
+        <Card.Title>Current EVIX Pool Price</Card.Title>
+        <Card.Text className="text-center">
+          {EVIXPoolPrice}
         </Card.Text>
       </Card.Body>
     </Card>
